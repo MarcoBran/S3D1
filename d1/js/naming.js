852f0c04112f4b5efb05de02122bdb7a39947b88
MarcoBran <114394974+MarcoBran@users.noreply.github.com>
2022-10-10 11:25:03 +0200

    Add files via upload

---
+++ d1/js/naming.js
@@ -0,0 +1,25 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+document.getElementById("prova").addEventListener("click", function () {
+    alert("ciao!");
+    console.log('alert chiuso')
+})
+document.getElementById("prefe").value=petPreferito;
+document.getElementById('saluto').innerHTML = PetPreferito;
+var pagina = 'sei sulla pagina index.html';
+console.log(pagina);
+
+var _pet2 = 'pantera';
+ document.getElementById('premio').addEventListener('click', function (){
+    document.write('<h1>VIRUS</h1>');
+    console.log('haivinto');
+ }) 
+
+//var 12 = 'numero';
+var numero = 12;
+console.log("il valore di 'numero' è = " + numero);
